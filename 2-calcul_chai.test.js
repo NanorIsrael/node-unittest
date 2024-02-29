c2389d579f2d22805aa59badfb8245ea0ebc6303
nanorisrael <israel.nanor@compozelabs.com>
2024-02-29 12:14:56 +0000

    Add sinon sample

--- 2-calcul_chai.test.js
+++ 2-calcul_chai.test.js
@@ -1,6 +1,6 @@
 const {expect} = require('chai')
 
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('Test calculator', () => {
 	describe("tests for SUM operations", () => {
