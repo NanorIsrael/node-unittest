14ebcf0785497b8c4c2e0ff60eaaaa4abd34209b
nanorisrael <israel.nanor@compozelabs.com>
2024-02-29 17:02:59 +0000

    Add skip example

--- 6-payment_token.test.js
+++ 6-payment_token.test.js
@@ -1,15 +1,14 @@
-const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe("getPaymentTokenFromAPI", () => {
 
 	it ("should return a resolved promise", (done) => {
 		getPaymentTokenFromAPI(true)
-		.then(done)
+		.then(() => done())
 	})
 
 	it ("should not return a resolved promise", (done) => {
 		getPaymentTokenFromAPI(false)
-		.then(done)
+		.then(() => done())
 	})
 })
